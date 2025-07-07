cc732c7331da0391fd057522f5e5ffbb110319a9
HaolinZhan <72926679+HaolinZhan@users.noreply.github.com>
2025-07-07 09:43:19 +0800

    Add files via upload

---
+++ DL_CW.py
@@ -0,0 +1,118 @@
+import torch
+import torch.nn as nn
+
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_channels, out_channels, stride=1):
+        super().__init__()
+        self.shrinkage = Shrinkage(out_channels, gap_size=(1))
+        # residual function
+        self.residual_function = nn.Sequential(
+            nn.Conv1d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False),
+            nn.BatchNorm1d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv1d(out_channels, out_channels * BasicBlock.expansion, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm1d(out_channels * BasicBlock.expansion),
+            self.shrinkage
+        )
+        # shortcut
+        self.shortcut = nn.Sequential()
+
+        # the shortcut output dimension is not the same with residual function
+        # use 1*1 convolution to match the dimension
+        if stride != 1 or in_channels != BasicBlock.expansion * out_channels:
+            self.shortcut = nn.Sequential(
+                nn.Conv1d(in_channels, out_channels * BasicBlock.expansion, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm1d(out_channels * BasicBlock.expansion)
+            )
+
+    def forward(self, x):
+
+         return nn.ReLU(inplace=True)(self.residual_function(x) + self.shortcut(x))
+
+
+class Shrinkage(nn.Module):
+    def __init__(self, channel, gap_size):
+        super(Shrinkage, self).__init__()
+        self.gap = nn.AdaptiveAvgPool1d(gap_size)
+        self.fc = nn.Sequential(
+            nn.Linear(channel, channel),
+            nn.BatchNorm1d(channel),
+            nn.ReLU(inplace=True),
+            nn.Linear(channel, channel),
+            nn.Sigmoid(),
+        )
+
+    def forward(self, x):
+        x_raw = x
+        x = torch.abs(x)
+        x_abs = x
+        x = self.gap(x)
+        x = torch.flatten(x, 1)
+        # average = torch.mean(x, dim=1, keepdim=True)  #CS
+        average = x    #CW
+        x = self.fc(x)
+        x = torch.mul(average, x)
+        x = x.unsqueeze(2)
+        # soft thresholding
+        sub = x_abs - x
+        zeros = sub - sub
+        n_sub = torch.max(sub, zeros)
+        x = torch.mul(torch.sign(x_raw), n_sub)
+        return x
+
+
+class RSNet(nn.Module):
+
+    def __init__(self, block, num_block, num_classes=1):
+        super().__init__()
+
+        self.in_channels = 64
+
+        self.conv1 = nn.Sequential(
+            nn.Conv1d(1, 64, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm1d(64),
+            nn.ReLU(inplace=True))
+
+        self.conv2_x = self._make_layer(block, 64, num_block[0], 1)
+        self.conv3_x = self._make_layer(block, 128, num_block[1], 1)
+        self.conv4_x = self._make_layer(block, 256, num_block[2], 1)
+        self.conv5_x = self._make_layer(block, 512, num_block[3], 1)
+
+        self.conv2 = nn.Sequential(
+            nn.Conv1d(512, 1, kernel_size=1, padding=0, bias=False),
+            nn.BatchNorm1d(1))
+
+    def _make_layer(self, block, out_channels, num_blocks, stride):
+
+
+        strides = [stride] + [1] * (num_blocks - 1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_channels, out_channels, stride))
+            self.in_channels = out_channels * block.expansion
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        output = self.conv1(x)
+        output = self.conv2_x(output)
+        output = self.conv3_x(output)
+        output = self.conv4_x(output)
+        output = self.conv5_x(output)
+        output = self.conv2(output)
+     #   print(output.size())
+
+
+
+
+        return output
+
+
+def rsnet18():
+    """ return a RSNet 18 object
+    """
+    return RSNet(BasicBlock, [2, 2, 2, 2])
+
